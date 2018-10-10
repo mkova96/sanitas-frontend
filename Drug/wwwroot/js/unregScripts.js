a042be6545f8697fd6aa87aa2899618fe74aea46
mkova <mario.kovacevic@fer.hr>
2018-10-10 18:28:18 +0200

    homepage made + some of navigation
    Signed-off-by: mkova <mario.kovacevic@fer.hr>

---
+++ Drug/wwwroot/js/unregScripts.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function() {
+    
+    /*SCROLL ON BUTTONS */   
+     $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    
+        
+});
